2876161c74e78a2cc4687f70ce00847fcee1405f
MAAREK ANTONY <maarekantony@gmail.com>
2026-01-12 13:38:35 -0500

    V4.0.1 - fix MinValueValidator import + clean docker startup

--- core/models.py
+++ core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator
+from django.db.models import Q
 class Symbol(models.Model):
     ticker = models.CharField(max_length=64)
     exchange = models.CharField(max_length=64, blank=True, default="")
